120c5e58258eb89121ad870d7d72e0d1d67ed84f
Kennon <kedusi@gmail.com>
2022-01-14 15:35:38 -0600

    install express; install puppeteer; basic screenshot app

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const http = require('http');
-
+const fs = require('fs');
+const { response } = require('express');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+const server = http.createServer((req, resp) => {
+    fs.readFile('index.html', function (error, pgResp) {
+        if (error) {
+            resp.writeHead(404);
+            resp.write('Contents you are looking are not found');
+        } else {
+            resp.writeHead(200, {'Content-Type': 'text/html'});
+            resp.write(pgResp);
+        }
+        resp.end();
+    });
 });
-
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at https://${hostname}:${port}`);
 });
---
+++ screenshot.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer');
+async function run () {
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto(url);
+    // await page.screenshot({path: 'screenshot.jpg'});
+    // browser.close();
+
+    console.log('Hello');
+}
